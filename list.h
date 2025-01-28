25bce2271042ce82836090a85b69b1aa967a2abd
Daniel Saghbini <134181843+saghbine@users.noreply.github.com>
2025-01-28 12:25:53 -0800

    Add files via upload

--- list.h
+++ list.h
@@ -33,55 +33,111 @@ class List
 {
     Node<T>* head;
     
-    void pop();
+    void remove(Node<T>* p){Node<T>* d=p->next; p->next=d->next; delete d;}
+    
+    int count()
+    {
+        int counter=0;
+        
+        for(Node<T>* it=head; it != NULL; it=it->next)
+            counter++;
+        
+        return counter;
+    }
     
 public:
     List(){head=NULL;}
-    Node<T>* push(const T &e){Node<T>* n=new Node<T>(e); n->next=head; head=n; return head;}
-    void Delete_Middle_Node(Node<T>* d);
     
     void show() const
     {
-        for(Node<T>* n=head; n != NULL; n=n->next)
-            cout<<n->data<<(n->next != NULL ? "->" : "");
-        
-        if(head != NULL)
-            cout<<endl;
+        for(Node<T>* it=head; it != NULL; it=it->next)
+            cout<<it->data<<(it->next != NULL ? "->" : "");
     }
     
-    ~List()
+    void Remove_Dups()
     {
-        while(head != NULL)
-            pop();
+        if(head == NULL || head->next == NULL) // if singly-linked-list is empty or just includes head
+            return;
+        
+        for(Node<T>* it1=head; it1->next != NULL; it1=it1->next) // for it1 @ head...(second-to-last)node in singly-linked-list
+            for(Node<T>* it2=it1; it2 != NULL; it2=it2->next) // for it2 @ it1...tail in singly-linked-list
+                while(it2->next != NULL && it1->data == it2->next->data) // while it2 isn't tail and it1 is equal to it2's next
+                    remove(it2); // unlink and delete it2's next from singly-linked-list
     }
+    
+    const T &Return_Kth_to_Last(int k) // k is kth-to-last position in singly-linked-list
+    {
+        int size=count(); // size is size of singly-linked-list
+        
+        if(k < 1 || k > size) // if k is invalid
+            throw "Function ended with exit code: "+ (head != NULL ? to_string(k) : to_string(0));
+        
+        Node<T>* kth=head; // kth points to head of singly-linked-list
+        
+        for(int c=size; c > k; kth=kth->next, c--); // kth points to kth-to-last node of singly-linked-list
+        
+        return kth->data; // return kth-to-last element in singly-linked-list
+    }
+    
+    void Delete_Middle_Node(Node<T>* dm) // dm is middle node in singly-linked-list to delete
+    {
+        if(head == NULL || dm == NULL || dm == head || dm->next == NULL) // if singly-linked-list is empty or dm is just invalid
+            return;
+        
+        Node<T>* it=head; // it points to head of singly-linked-list
+        
+        for(; it->next != NULL && it->next != dm; it=it->next); // it points to tail or node right before dm in singly-linked-list
+        
+        if(it->next != NULL) // if it points to node right before dm in singly-linked-list
+            remove(it); // unlink and delete it's next from singly-linked-list
+    }
+    
+    void pull(const T &e);
+    Node<T>* npull(const T &e);
+    ~List();
 };
 
 template<class T>
-void List<T>::Delete_Middle_Node(Node<T>* d) // d is node to delete in singly linked list
+void List<T>::pull(const T &e)
 {
-    if(head != NULL && d != NULL && d != head && d->next != NULL) // d can't be head or tail in singly linked list
+    Node<T>* n=new Node<T>(e);
+    
+    if(head == NULL)
+        head=n;
+    else
     {
-        Node<T>* p; // temporary node pointer p
+        Node<T>* it=head;
         
-        for(p=head; p != NULL && p->next != d; p=p->next); // check p at head...tail in singly linked list to stop at d if available
-        
-        if(p != NULL) // d was available
-        {
-            p->next=d->next; // fix link in singly linked list
-            delete d; // delete d from singly linked list
-        }
+        for(; it->next != NULL; it=it->next);
+        it->next=n;
     }
 }
 
 template<class T>
-void List<T>::pop()
+Node<T>* List<T>::npull(const T &e)
 {
-    if(head != NULL)
+    Node<T>* n=new Node<T>(e);
+    
+    if(head == NULL)
+        return head=n;
+    else
     {
-        Node<T>* n=head->next;
+        Node<T>* it=head;
+        
+        for(; it->next != NULL; it=it->next);
+        return it->next=n;
+    }
+}
+
+template<class T>
+List<T>::~List()
+{
+    while(head != NULL)
+    {
+        Node<T>* it=head->next;
         
         delete head;
-        head=n;
+        head=it;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -7,29 +7,37 @@
 
 #include "list.h"
 
+Node<int>* initializer(List<int> &l);
+
 int main()
 {
     cout<<"problem\n-------\n\tImplement an algorithm to delete a node in the middle (i.e., any node but the first and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.\n\n";
     
     cout<<"input\n-----\n\t";
     List<int> l;
-    Node<int>* n=NULL;
     srand(int(time(0)));
-    int mid=rand()%8+2;
+    Node<int>* np=initializer(l);
     int temp;
-    
-    for(int i=1; i <= 10; i++)
-        i == mid ? n=l.push(rand()%20+1) : l.push(rand()%20+1);
-    
     l.show();
-    cout<<"\tDelete middle node "<<(temp=n->info())<<"!\n\n";
+    cout<<"\n\tDelete middle node "<<(temp=np->info())<<"!\n\n";
     
-    l.Delete_Middle_Node(n);
+    l.Delete_Middle_Node(np);
     
     cout<<"output\n------\n\tDeleted middle node "<<temp<<".\n\t";
     l.show();
     
-    cout<<"\nsolution\n--------\n\tAuxiliary space complexity: O(1); time complexity: O(n).\n\n";
+    cout<<"\n\nsolution\n--------\n\tAuxiliary space complexity: O(1); time complexity: O(n).\n\n";
     
     return 0;
+}
+
+Node<int>* initializer(List<int> &l)
+{
+    int mid=rand()%8+2;
+    Node<int>* np=NULL;
+    
+    for(int i=1; i <= 10; i++)
+        i == mid ? np=l.npull(rand()%20+1) : l.npull(rand()%20+1);
+    
+    return np;
 }
